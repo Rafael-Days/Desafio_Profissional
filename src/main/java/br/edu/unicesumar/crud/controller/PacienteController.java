0521dd6306adb48aff91a1c2dc832df646bfdf12
Rafael Days <rafael.db.2003@gmail.com>
2023-10-25 16:03:11 -0300

    commit teste

--- src/main/java/br/edu/unicesumar/crud/controller/PacienteController.java
+++ src/main/java/br/edu/unicesumar/crud/controller/PacienteController.java
@@ -1,7 +1,6 @@
 package br.edu.unicesumar.crud.controller;
 
 import br.edu.unicesumar.crud.model.repository.PacienteRepository;
-import br.edu.unicesumar.crud.model.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
