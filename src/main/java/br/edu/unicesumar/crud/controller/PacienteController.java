a4a4441d6deb0738bbc697a404f0676ad3158424
Rafael Days <rafael.db.2003@gmail.com>
2023-10-25 16:10:11 -0300

    teste

--- src/main/java/br/edu/unicesumar/crud/controller/PacienteController.java
+++ src/main/java/br/edu/unicesumar/crud/controller/PacienteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/paciente")
 public class PacienteController {
-//
+
     @Autowired
     private PacienteRepository pacienteRepository;
 }
